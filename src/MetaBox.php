21b5e4955adb05bbe7782c07e7430f072c7d1fdd
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-08-21 20:02:56 +0000

    Set default attribute value in the appropriate type

--- src/MetaBox.php
+++ src/MetaBox.php
@@ -218,7 +218,7 @@ class MetaBox
         unset($vars['nonce']);
 
         foreach ($vars as $key => $value) {
-            $this->{$key} = $args[$key] ?? '';
+            $this->{$key} = $args[$key] ?? null;
         }
     }
 
@@ -233,12 +233,12 @@ class MetaBox
             'normal',
             'side',
             'advanced'
-        ]) ? $this->context : null);
+        ]) ? $this->context : '');
 
         $this->priority = (\in_array(
             $this->priority,
             ['high', 'low']
-        ) ? $this->priority : null);
+        ) ? $this->priority : '');
     }
 
     /**
